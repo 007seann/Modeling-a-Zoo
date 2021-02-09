14a78a1fc2484c939c62838f2417045b1873a928
Ky Lee <kyulee@live.com>
2021-02-09 15:15:50 -0800

    Move build.sh and run.sh to the top directory

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -x
 
 export JAVA_HOME=`/usr/libexec/java_home -v 13`
 
-$JAVA_HOME/bin/java ConnectFour
+$JAVA_HOME/bin/javac Assignment1/*.java
--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ set -x
 
 export JAVA_HOME=`/usr/libexec/java_home -v 13`
 
-$JAVA_HOME/bin/javac *.java
+$JAVA_HOME/bin/java Assignment1/ConnectFour
